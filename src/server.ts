d74e25caddec42f14b4590008c990fe79a0d3f7e
shihabsararrafid <shrafid.532@gmail.com>
2023-06-06 12:49:55 +0600

    [DONE] setup cors

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import app from "./app";
+import config from "./../config/index";
+async function bootstrap() {
+  try {
+    await mongoose.connect(config.database_url as string);
+    console.log("Database Connection Successful");
+    app.listen(config.port, () => {
+      console.log(`Example app listening on port ${config.port}`);
+    });
+  } catch (error: any) {
+    console.log(`Failed to connect due to  ${error.message}`);
+  }
+}
+bootstrap();
