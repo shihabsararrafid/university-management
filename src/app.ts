2bd2aa128602aeb0c64032a13a875027b0bf1861
shihabsararrafid <shrafid.532@gmail.com>
2023-06-07 10:48:39 +0600

    testing

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,10 @@
-import express, { Application } from "express";
-import cors from "cors";
-const app: Application = express();
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-export default app;
+import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
+const app: Application = express()
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!')
+})
+export default app
